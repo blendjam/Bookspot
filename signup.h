2013bfef40f261f37bf9ae99c5ec436039a2b5d5
heriz00 <65176578+heriz00@users.noreply.github.com>
2020-08-06 23:23:02 +0545

    Create signup.h

---
+++ signup.h
@@ -0,0 +1,33 @@
+#ifndef SIGNUP_H
+#define SIGNUP_H
+
+#include <QDialog>
+#include <QtSql>
+#include <QString>
+
+namespace Ui {
+class Signup;
+}
+
+class Signup : public QDialog
+{
+    Q_OBJECT
+
+public:
+    QSqlDatabase userinfo;
+
+public:
+    bool dbOpen();
+    void dbClose();
+public:
+    explicit Signup(QWidget *parent = nullptr);
+    ~Signup();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Signup *ui;
+};
+
+#endif // SIGNUP_H
